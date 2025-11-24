e7270f5b200e02be3ac71b44ab5626412d6fcf26
Violet <octops64@gmail.com>
2025-11-24 15:31:37 +0000

    Added visual tree for local folder and file structure

---
+++ Folder.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using HarfBuzzSharp;
+
+namespace ViSync;
+
+public class Folder
+{
+    public ObservableCollection<Folder>? SubFolders { get; }
+    public string Title { get; }
+    public string Path { get; }
+    
+    private Folder(string title, string path, ObservableCollection<Folder>? subFolders)
+    {
+        Title = title;
+        SubFolders = subFolders;
+        Path = path;
+    }
+
+    public Folder(string path)
+    {
+        if (!Directory.Exists(path)) throw new DirectoryNotFoundException();
+
+        var mainDir = new DirectoryInfo(path);
+
+        var madeFolder = MakeFolder(mainDir);
+
+        Title = madeFolder.Title;
+        Path = madeFolder.Path;
+        SubFolders = madeFolder.SubFolders;
+    }
+
+    private static Folder MakeFolder(DirectoryInfo parentFolder)
+    {
+        var subFolders = new ObservableCollection<Folder>();
+        
+        foreach (var folder in parentFolder.GetDirectories())
+        {
+            subFolders.Add(MakeFolder(folder));
+        }
+
+        foreach (var file in parentFolder.GetFiles())
+        {
+            subFolders.Add(new Folder(file.Name, file.FullName, null));
+        }
+
+        return new Folder(parentFolder.Name, parentFolder.FullName, subFolders);
+    }
+}
--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,14 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Templates;
 
 namespace ViSync;
 
 public partial class MainWindow : Window
 {
+    private ObservableCollection<Folder>? _localFolder;
+    public ObservableCollection<Folder> LocalFolder
+    {
+        get => _localFolder ?? [];
+
+        set
+        {
+            _localFolder = value;
+            LocalFolderTree.ItemsSource = _localFolder;
+        }
+    }
+
+    private string LocalPath { get; set; } = "D:/Music/Main";
+    
     public MainWindow()
     {
         InitializeComponent();
-
-        LocalPathInputBox.Text = "D:/example/folder/with/files";
-        ToolTip.SetTip(LocalPathInputBox, LocalPathInputBox.Text);
+        
+        SetupLocalPath();
     }
+
+    private void SetupLocalPath()
+    {
+        LocalPathInputBox.Text = LocalPath;
+        ToolTip.SetTip(LocalPathInputBox, LocalPathInputBox.Text);
+
+        LocalFolder = [new Folder(LocalPath)];
+    }
+
 }
