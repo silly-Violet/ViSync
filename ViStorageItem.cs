384fd8d0287429520fd7ad776a58f49029864144
Violet <octops64@gmail.com>
2025-11-29 01:37:46 +0000

    Fixed naming inconsistency

--- ViStorageItem.cs
+++ ViStorageItem.cs
@@ -6,7 +6,7 @@ namespace ViSync;
 
 public class ViStorageItem
 {
-    public ObservableCollection<ViStorageItem>? SubFolders { get; }
+    public ObservableCollection<ViStorageItem>? SubItems { get; }
     public string Title { get; }
     public string Path { get; }
     public int Type { get; }
@@ -18,12 +18,12 @@ public class ViStorageItem
     /// <param name="title"></param>
     /// <param name="path"></param>
     /// <param name="type">0 == Folder, 1 == File</param>
-    /// <param name="subFolders"></param>
+    /// <param name="subItems"></param>
     /// <param name="hash"></param>
-    private ViStorageItem(string title, string path, int type, ObservableCollection<ViStorageItem>? subFolders, string? hash = null)
+    private ViStorageItem(string title, string path, int type, ObservableCollection<ViStorageItem>? subItems, string? hash = null)
     {
         Title = title;
-        SubFolders = subFolders;
+        SubItems = subItems;
         Path = path;
         Type = type;
         Hash = hash;
@@ -39,26 +39,26 @@ public class ViStorageItem
 
         Title = madeFolder.Title;
         Path = madeFolder.Path;
-        SubFolders = madeFolder.SubFolders;
+        SubItems = madeFolder.SubItems;
     }
 
     private static ViStorageItem MakeFolder(DirectoryInfo parentFolder)
     {
-        var subFolders = new ObservableCollection<ViStorageItem>();
+        var subItems = new ObservableCollection<ViStorageItem>();
         
         foreach (var folder in parentFolder.GetDirectories())
         {
-            subFolders.Add(MakeFolder(folder));
+            subItems.Add(MakeFolder(folder));
         }
 
         //todo: order by numerical order (look at gallery)
         
         foreach (var file in parentFolder.GetFiles())
         {
-            subFolders.Add(new ViStorageItem("📄" + file.Name, file.FullName, 1,null, GetHashString(file)));
+            subItems.Add(new ViStorageItem("📄" + file.Name, file.FullName, 1,null, GetHashString(file)));
         }
 
-        return new ViStorageItem("📁" + parentFolder.Name, parentFolder.FullName, 0, subFolders);
+        return new ViStorageItem("📁" + parentFolder.Name, parentFolder.FullName, 0, subItems);
     }
 
     private static string GetHashString(FileInfo file)
